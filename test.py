0eccb10bb92a75110908dcfb60af335773e1a3c8
Mogician <Mogician@zju.edu.cn>
2021-07-12 20:33:16 +0800

    233

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
-import numpy as np 
-import pandas as pd
-a=pd.read_csv("CN-Reanalysis-daily-2013010100.csv")
-a.columns=['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3', 'U(m/s)', 'V(m/s)', 'TEMP(K)', 'RH(%)', 'PSFC(Pa)', 'lat', 'lon','date']
-#print(a[:5])
-a['date']="2013-01-01"
-a.to_csv('res.csv')
-'''a=pd.read_csv('1.csv')
-b=pd.read_csv('2.csv')
-a=a.append(b)
-print(a)
-a.to_csv('res.csv')'''
+import requests
+from requests.api import request
+#r = requests.get('https://www.douban.com/search', params={'q': 'python', 'cat': '1001'})
+r=requests.get('https://restapi.amap.com/v3/geocode/regeo',params={'key':'a1f20e56788bc5e9930326306872d63e','location':'111.43,21.97'})
+j=r.json()
+print(j)
+print(j['regeocode']['addressComponent']['province'])
